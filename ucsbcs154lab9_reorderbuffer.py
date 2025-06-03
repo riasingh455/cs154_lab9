1e1f5cc33b450b1be536b2fd9629867ee468fbea
riasingh455 <riasingh@ucsb.edu>
2025-06-03 08:46:48 -0700

    cfnwj

--- ucsbcs154lab9_reorderbuffer.py
+++ ucsbcs154lab9_reorderbuffer.py
@@ -33,42 +33,79 @@ rob_preg = pyrtl.MemBlock(bitwidth=5, addrwidth=4, name="rob_preg")
 
 #---------------------------------------------------------------------------------------
 
-h = pyrtl.Register(4, name='rob_h') #oldest val entry
-t = pyrtl.Register(4, name='rob_t') #next free
-count = pyrtl.Register(5, name='rob_count') #valid entries
+# h = pyrtl.Register(4, name='rob_h') #oldest val entry
+# t = pyrtl.Register(4, name='rob_t') #next free
+# count = pyrtl.Register(5, name='rob_count') #valid entries
 
-full = count == 16
-empty = count == 0
+# full = count == 16
+# empty = count == 0
 
-head_is_being_filled = rob_fill_val_i & (h == rob_fill_slot_i)
-entry_ready = rob_valid[h] & ~rob_pending[h] & ~head_is_being_filled
-commit_fire = (~empty) & entry_ready       
+# head_is_being_filled = rob_fill_val_i & (h == rob_fill_slot_i)
+# entry_ready = rob_valid[h] & ~rob_pending[h] & ~head_is_being_filled
+# commit_fire = (~empty) & entry_ready       
 
-space_avail = count < 16
-rob_alloc_req_rdy_o <<= space_avail           
-alloc_fire = rob_alloc_req_val_i & space_avail  
+# space_avail = count < 16
+# rob_alloc_req_rdy_o <<= space_avail           
+# alloc_fire = rob_alloc_req_val_i & space_avail  
 
-rob_alloc_resp_slot_o <<= t
+# rob_alloc_resp_slot_o <<= t
 
-rob_commit_wen_o <<= commit_fire
+# rob_commit_wen_o <<= commit_fire
+# rob_commit_slot_o <<= h
+# rob_commit_rf_waddr_o <<= rob_preg[h]
+
+# with pyrtl.conditional_assignment:
+#     with alloc_fire:
+#         rob_valid[t]<<= 1
+#         rob_pending[t] <<= 1
+#         rob_preg[t] <<= rob_alloc_req_preg_i
+#     with rob_fill_val_i:
+#         rob_pending[rob_fill_slot_i] <<= 0
+
+# t_next = pyrtl.select(t == 15, 0, t+1)
+# h_next = pyrtl.select(h == 15, 0, h+1)
+
+# t.next <<= pyrtl.select(alloc_fire, t_next, t)    
+# h.next <<= pyrtl.select(commit_fire, h_next, h)
+
+# count.next <<= count + alloc_fire - commit_fire
+
+
+
+h = pyrtl.Register(bitwidth=4, name='h')
+t = pyrtl.Register(bitwidth=4, name='t')
+
+s_avail = ~rob_valid[t]
+
+rob_alloc_req_rdy_o <<= s_avail
+t.next <<= t+1     
+
+alloc_fire = s_avail & rob_alloc_req_val_i
+with pyrtl.conditional_assignment:
+    with alloc_fire:
+        rob_pending[t] |= 1
+        rob_valid[t] |= 1
+        rob_preg[t] |= rob_alloc_req_preg_i
+        rob_alloc_resp_slot_o |= t
+    with rob_fill_val_i:
+        rob_pending[rob_fill_slot_i] |= 0
+
+commit_fire = rob_valid[h] & ~rob_pending[h]
+
+with pyrtl.conditional_assignment:
+    with commit_fire:
+        rob_commit_wen_o |= 1
+        rob_valid[h] |= 0
+        h_is_last = h == 15
+        with h_is_last:
+            h.next |= 0
+        with pyrtl.otherwise:
+            h.next |= h + 1
+    with ~commit_fire:
+        rob_commit_wen_o |= 0
 rob_commit_slot_o <<= h
 rob_commit_rf_waddr_o <<= rob_preg[h]
 
-with pyrtl.conditional_assignment:
-    with alloc_fire:
-        rob_valid[t]<<= 1
-        rob_pending[t] <<= 1
-        rob_preg[t] <<= rob_alloc_req_preg_i
-    with rob_fill_val_i:
-        rob_pending[rob_fill_slot_i] <<= 0
-
-t_next = pyrtl.select(t == 15, 0, t+1)
-h_next = pyrtl.select(h == 15, 0, h+1)
-
-t.next <<= pyrtl.select(alloc_fire, t_next, t)    
-h.next <<= pyrtl.select(commit_fire, h_next, h)
-
-count.next <<= count + alloc_fire - commit_fire
 
 
 #---------------------------------------------------------------------------------------
